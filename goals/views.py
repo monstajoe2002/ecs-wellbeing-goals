4c45fac34a32374a94eccaf372d852467c82ac55
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-05-24 20:09:29 +0300

    Diary view created

--- goals/views.py
+++ goals/views.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
 # Create your views here.
-from .models import Goal
-from .serializers import GoalSerializer
+from .models import Diary, Goal
+from .serializers import DiarySerializer, GoalSerializer
 
 
 class GoalViewSet(viewsets.ModelViewSet):
 
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
+
+class DiaryViewSet(viewsets.ModelViewSet):
+
+    queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
